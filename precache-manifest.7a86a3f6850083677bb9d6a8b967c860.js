73f86063a4e2994e3b20ffeb85d1f26d1bc58748
James Ravenscroft <hello@ravenscroftgames.net>
2020-01-13 16:08:37 -0800

    Updates

---
+++ precache-manifest.7a86a3f6850083677bb9d6a8b967c860.js
@@ -0,0 +1,466 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2583ed9f3381e1967254d605d0c10282",
+    "url": "/pure-react-animalchemists/index.html"
+  },
+  {
+    "revision": "49b60b7b1413a14e9fc6",
+    "url": "/pure-react-animalchemists/static/css/main.d5698203.chunk.css"
+  },
+  {
+    "revision": "84d5068fafbf8b19df76",
+    "url": "/pure-react-animalchemists/static/js/2.50a6326c.chunk.js"
+  },
+  {
+    "revision": "49b60b7b1413a14e9fc6",
+    "url": "/pure-react-animalchemists/static/js/main.50f63729.chunk.js"
+  },
+  {
+    "revision": "64d35ca6e4cb95605f17",
+    "url": "/pure-react-animalchemists/static/js/runtime-main.ab54a29e.js"
+  },
+  {
+    "revision": "2f996ad84bde5d1cf975575c54164a11",
+    "url": "/pure-react-animalchemists/static/media/49.2f996ad8.png"
+  },
+  {
+    "revision": "7abee62a84e50080b98a5b0a5c66e641",
+    "url": "/pure-react-animalchemists/static/media/Awaken.7abee62a.png"
+  },
+  {
+    "revision": "0bc2dca64e4bb83a595d692367aa32c9",
+    "url": "/pure-react-animalchemists/static/media/Bone - Copy.0bc2dca6.png"
+  },
+  {
+    "revision": "0bc2dca64e4bb83a595d692367aa32c9",
+    "url": "/pure-react-animalchemists/static/media/Bone.0bc2dca6.png"
+  },
+  {
+    "revision": "0bc2dca64e4bb83a595d692367aa32c9",
+    "url": "/pure-react-animalchemists/static/media/Bone0.0bc2dca6.png"
+  },
+  {
+    "revision": "0bc2dca64e4bb83a595d692367aa32c9",
+    "url": "/pure-react-animalchemists/static/media/Bone1.0bc2dca6.png"
+  },
+  {
+    "revision": "0bc2dca64e4bb83a595d692367aa32c9",
+    "url": "/pure-react-animalchemists/static/media/Bone2.0bc2dca6.png"
+  },
+  {
+    "revision": "0bc2dca64e4bb83a595d692367aa32c9",
+    "url": "/pure-react-animalchemists/static/media/Bone3.0bc2dca6.png"
+  },
+  {
+    "revision": "0bc2dca64e4bb83a595d692367aa32c9",
+    "url": "/pure-react-animalchemists/static/media/Bone4.0bc2dca6.png"
+  },
+  {
+    "revision": "0bc2dca64e4bb83a595d692367aa32c9",
+    "url": "/pure-react-animalchemists/static/media/Bone5.0bc2dca6.png"
+  },
+  {
+    "revision": "0bc2dca64e4bb83a595d692367aa32c9",
+    "url": "/pure-react-animalchemists/static/media/Bone6.0bc2dca6.png"
+  },
+  {
+    "revision": "0bc2dca64e4bb83a595d692367aa32c9",
+    "url": "/pure-react-animalchemists/static/media/Bone7.0bc2dca6.png"
+  },
+  {
+    "revision": "0bc2dca64e4bb83a595d692367aa32c9",
+    "url": "/pure-react-animalchemists/static/media/Bone8.0bc2dca6.png"
+  },
+  {
+    "revision": "0bc2dca64e4bb83a595d692367aa32c9",
+    "url": "/pure-react-animalchemists/static/media/Bone9.0bc2dca6.png"
+  },
+  {
+    "revision": "95219294882258cd069374e1513b61f7",
+    "url": "/pure-react-animalchemists/static/media/Confuse.95219294.png"
+  },
+  {
+    "revision": "9f0189eebae6c3eaa4c45d4bf1342a14",
+    "url": "/pure-react-animalchemists/static/media/Darkness.9f0189ee.png"
+  },
+  {
+    "revision": "02cf2d6e8989b5c1a405b222671ffdfa",
+    "url": "/pure-react-animalchemists/static/media/Disrupt.02cf2d6e.png"
+  },
+  {
+    "revision": "06396d2c750d6ef00f0f63aa334dde94",
+    "url": "/pure-react-animalchemists/static/media/Echo.06396d2c.png"
+  },
+  {
+    "revision": "5f547a01cbd7d4c37e6965b278414405",
+    "url": "/pure-react-animalchemists/static/media/Endure.5f547a01.png"
+  },
+  {
+    "revision": "25a1bf87631f82bcd2bd0059e3428a07",
+    "url": "/pure-react-animalchemists/static/media/Essence0.25a1bf87.png"
+  },
+  {
+    "revision": "25a1bf87631f82bcd2bd0059e3428a07",
+    "url": "/pure-react-animalchemists/static/media/Essence1.25a1bf87.png"
+  },
+  {
+    "revision": "25a1bf87631f82bcd2bd0059e3428a07",
+    "url": "/pure-react-animalchemists/static/media/Essence2.25a1bf87.png"
+  },
+  {
+    "revision": "25a1bf87631f82bcd2bd0059e3428a07",
+    "url": "/pure-react-animalchemists/static/media/Essence3.25a1bf87.png"
+  },
+  {
+    "revision": "a0c48d9407ea4245dfd55d5417156597",
+    "url": "/pure-react-animalchemists/static/media/EssenceBack2.a0c48d94.png"
+  },
+  {
+    "revision": "206b1d03843af616a165d7e888d870eb",
+    "url": "/pure-react-animalchemists/static/media/Extract0.206b1d03.png"
+  },
+  {
+    "revision": "206b1d03843af616a165d7e888d870eb",
+    "url": "/pure-react-animalchemists/static/media/Extract1.206b1d03.png"
+  },
+  {
+    "revision": "206b1d03843af616a165d7e888d870eb",
+    "url": "/pure-react-animalchemists/static/media/Extract2.206b1d03.png"
+  },
+  {
+    "revision": "206b1d03843af616a165d7e888d870eb",
+    "url": "/pure-react-animalchemists/static/media/Extract3.206b1d03.png"
+  },
+  {
+    "revision": "9b6616ac8302b7429199fcfeb85fc6a6",
+    "url": "/pure-react-animalchemists/static/media/ExtractBack.9b6616ac.png"
+  },
+  {
+    "revision": "ecc0d5550df583797fddf108dfaa4777",
+    "url": "/pure-react-animalchemists/static/media/Fear.ecc0d555.png"
+  },
+  {
+    "revision": "9cb699400d26b368d9cb7624a3bb132f",
+    "url": "/pure-react-animalchemists/static/media/FennecFox.9cb69940.png"
+  },
+  {
+    "revision": "73775ff1f98c9a90596e8014809ec237",
+    "url": "/pure-react-animalchemists/static/media/FennecFoxBack.73775ff1.png"
+  },
+  {
+    "revision": "2b65ee3ad348a6d7ec9e51e4f2e0bb98",
+    "url": "/pure-react-animalchemists/static/media/Foresight.2b65ee3a.png"
+  },
+  {
+    "revision": "87cd885556d71a906cd318f2edeee6b5",
+    "url": "/pure-react-animalchemists/static/media/Herbs0.87cd8855.png"
+  },
+  {
+    "revision": "87cd885556d71a906cd318f2edeee6b5",
+    "url": "/pure-react-animalchemists/static/media/Herbs1.87cd8855.png"
+  },
+  {
+    "revision": "87cd885556d71a906cd318f2edeee6b5",
+    "url": "/pure-react-animalchemists/static/media/Herbs2.87cd8855.png"
+  },
+  {
+    "revision": "87cd885556d71a906cd318f2edeee6b5",
+    "url": "/pure-react-animalchemists/static/media/Herbs3.87cd8855.png"
+  },
+  {
+    "revision": "87cd885556d71a906cd318f2edeee6b5",
+    "url": "/pure-react-animalchemists/static/media/Herbs4.87cd8855.png"
+  },
+  {
+    "revision": "87cd885556d71a906cd318f2edeee6b5",
+    "url": "/pure-react-animalchemists/static/media/Herbs5.87cd8855.png"
+  },
+  {
+    "revision": "87cd885556d71a906cd318f2edeee6b5",
+    "url": "/pure-react-animalchemists/static/media/Herbs6.87cd8855.png"
+  },
+  {
+    "revision": "87cd885556d71a906cd318f2edeee6b5",
+    "url": "/pure-react-animalchemists/static/media/Herbs7.87cd8855.png"
+  },
+  {
+    "revision": "87cd885556d71a906cd318f2edeee6b5",
+    "url": "/pure-react-animalchemists/static/media/Herbs8.87cd8855.png"
+  },
+  {
+    "revision": "87cd885556d71a906cd318f2edeee6b5",
+    "url": "/pure-react-animalchemists/static/media/Herbs9.87cd8855.png"
+  },
+  {
+    "revision": "303997838e05e6b4d313c72452188d5f",
+    "url": "/pure-react-animalchemists/static/media/Honey.30399783.png"
+  },
+  {
+    "revision": "303997838e05e6b4d313c72452188d5f",
+    "url": "/pure-react-animalchemists/static/media/Honey0.30399783.png"
+  },
+  {
+    "revision": "303997838e05e6b4d313c72452188d5f",
+    "url": "/pure-react-animalchemists/static/media/Honey1.30399783.png"
+  },
+  {
+    "revision": "303997838e05e6b4d313c72452188d5f",
+    "url": "/pure-react-animalchemists/static/media/Honey2.30399783.png"
+  },
+  {
+    "revision": "303997838e05e6b4d313c72452188d5f",
+    "url": "/pure-react-animalchemists/static/media/Honey3.30399783.png"
+  },
+  {
+    "revision": "303997838e05e6b4d313c72452188d5f",
+    "url": "/pure-react-animalchemists/static/media/Honey4.30399783.png"
+  },
+  {
+    "revision": "303997838e05e6b4d313c72452188d5f",
+    "url": "/pure-react-animalchemists/static/media/Honey5.30399783.png"
+  },
+  {
+    "revision": "303997838e05e6b4d313c72452188d5f",
+    "url": "/pure-react-animalchemists/static/media/Honey6.30399783.png"
+  },
+  {
+    "revision": "303997838e05e6b4d313c72452188d5f",
+    "url": "/pure-react-animalchemists/static/media/Honey7.30399783.png"
+  },
+  {
+    "revision": "303997838e05e6b4d313c72452188d5f",
+    "url": "/pure-react-animalchemists/static/media/Honey8.30399783.png"
+  },
+  {
+    "revision": "303997838e05e6b4d313c72452188d5f",
+    "url": "/pure-react-animalchemists/static/media/Honey9.30399783.png"
+  },
+  {
+    "revision": "921a9bcdb35b2f01910854709ca87189",
+    "url": "/pure-react-animalchemists/static/media/HoneyBadger Back.921a9bcd.png"
+  },
+  {
+    "revision": "9bb34402a234c18a2671ad0e131b0c85",
+    "url": "/pure-react-animalchemists/static/media/HoneyBadger.9bb34402.png"
+  },
+  {
+    "revision": "2a9aa5e2d0ab485e1f6a75bf6a0bd6fb",
+    "url": "/pure-react-animalchemists/static/media/Incense0.2a9aa5e2.png"
+  },
+  {
+    "revision": "2a9aa5e2d0ab485e1f6a75bf6a0bd6fb",
+    "url": "/pure-react-animalchemists/static/media/Incense1.2a9aa5e2.png"
+  },
+  {
+    "revision": "2a9aa5e2d0ab485e1f6a75bf6a0bd6fb",
+    "url": "/pure-react-animalchemists/static/media/Incense2.2a9aa5e2.png"
+  },
+  {
+    "revision": "2a9aa5e2d0ab485e1f6a75bf6a0bd6fb",
+    "url": "/pure-react-animalchemists/static/media/Incense3.2a9aa5e2.png"
+  },
+  {
+    "revision": "0a6e0a582080a8835b74bd244ba078c7",
+    "url": "/pure-react-animalchemists/static/media/IncenseBack2.0a6e0a58.png"
+  },
+  {
+    "revision": "5732e6f488b2fe7a1e2997b5802b569c",
+    "url": "/pure-react-animalchemists/static/media/IngredientBacks.5732e6f4.png"
+  },
+  {
+    "revision": "e9d0cde2097c144cbae1d9f90e1da83a",
+    "url": "/pure-react-animalchemists/static/media/Invisibility.e9d0cde2.png"
+  },
+  {
+    "revision": "9ebd7dae1a74ca7937b95e9f0db30151",
+    "url": "/pure-react-animalchemists/static/media/Mushroom.9ebd7dae.png"
+  },
+  {
+    "revision": "9ebd7dae1a74ca7937b95e9f0db30151",
+    "url": "/pure-react-animalchemists/static/media/Mushroom0.9ebd7dae.png"
+  },
+  {
+    "revision": "9ebd7dae1a74ca7937b95e9f0db30151",
+    "url": "/pure-react-animalchemists/static/media/Mushroom1.9ebd7dae.png"
+  },
+  {
+    "revision": "9ebd7dae1a74ca7937b95e9f0db30151",
+    "url": "/pure-react-animalchemists/static/media/Mushroom2.9ebd7dae.png"
+  },
+  {
+    "revision": "9ebd7dae1a74ca7937b95e9f0db30151",
+    "url": "/pure-react-animalchemists/static/media/Mushroom3.9ebd7dae.png"
+  },
+  {
+    "revision": "9ebd7dae1a74ca7937b95e9f0db30151",
+    "url": "/pure-react-animalchemists/static/media/Mushroom4.9ebd7dae.png"
+  },
+  {
+    "revision": "9ebd7dae1a74ca7937b95e9f0db30151",
+    "url": "/pure-react-animalchemists/static/media/Mushroom5.9ebd7dae.png"
+  },
+  {
+    "revision": "9ebd7dae1a74ca7937b95e9f0db30151",
+    "url": "/pure-react-animalchemists/static/media/Mushroom6.9ebd7dae.png"
+  },
+  {
+    "revision": "9ebd7dae1a74ca7937b95e9f0db30151",
+    "url": "/pure-react-animalchemists/static/media/Mushroom7.9ebd7dae.png"
+  },
+  {
+    "revision": "9ebd7dae1a74ca7937b95e9f0db30151",
+    "url": "/pure-react-animalchemists/static/media/Mushroom8.9ebd7dae.png"
+  },
+  {
+    "revision": "9ebd7dae1a74ca7937b95e9f0db30151",
+    "url": "/pure-react-animalchemists/static/media/Mushroom9.9ebd7dae.png"
+  },
+  {
+    "revision": "79feb0640ef44eacc92bfb0bfe87ab88",
+    "url": "/pure-react-animalchemists/static/media/Pangolin.79feb064.png"
+  },
+  {
+    "revision": "7f21053f99cf4adb84a38c005f8daada",
+    "url": "/pure-react-animalchemists/static/media/PangolinBack.7f21053f.png"
+  },
+  {
+    "revision": "0d48ce58966703a6da9a577eba529bab",
+    "url": "/pure-react-animalchemists/static/media/Plague.0d48ce58.png"
+  },
+  {
+    "revision": "4ed9e29249008f1acc141c6bb2d2e52c",
+    "url": "/pure-react-animalchemists/static/media/Poison0.4ed9e292.png"
+  },
+  {
+    "revision": "4ed9e29249008f1acc141c6bb2d2e52c",
+    "url": "/pure-react-animalchemists/static/media/Poison1.4ed9e292.png"
+  },
+  {
+    "revision": "4ed9e29249008f1acc141c6bb2d2e52c",
+    "url": "/pure-react-animalchemists/static/media/Poison2.4ed9e292.png"
+  },
+  {
+    "revision": "4ed9e29249008f1acc141c6bb2d2e52c",
+    "url": "/pure-react-animalchemists/static/media/Poison3.4ed9e292.png"
+  },
+  {
+    "revision": "7326b724831e4f299744f2ead702f5ad",
+    "url": "/pure-react-animalchemists/static/media/PoisonBack2.7326b724.png"
+  },
+  {
+    "revision": "f8e14d2826fbe0c4ea841c37dd6340eb",
+    "url": "/pure-react-animalchemists/static/media/RedPanda.f8e14d28.png"
+  },
+  {
+    "revision": "762614bd2ee4e4f90a5705dcde817579",
+    "url": "/pure-react-animalchemists/static/media/RedPandaBack.762614bd.png"
+  },
+  {
+    "revision": "f140e9f5b6c5b430bbdd8de396ebecd0",
+    "url": "/pure-react-animalchemists/static/media/Restore.f140e9f5.png"
+  },
+  {
+    "revision": "31a06b65c32e4ca8c266945d141ae7bb",
+    "url": "/pure-react-animalchemists/static/media/Scorch.31a06b65.png"
+  },
+  {
+    "revision": "e542cabb75587c13d9aa0a8fc4569222",
+    "url": "/pure-react-animalchemists/static/media/Seeds0.e542cabb.png"
+  },
+  {
+    "revision": "e542cabb75587c13d9aa0a8fc4569222",
+    "url": "/pure-react-animalchemists/static/media/Seeds1.e542cabb.png"
+  },
+  {
+    "revision": "e542cabb75587c13d9aa0a8fc4569222",
+    "url": "/pure-react-animalchemists/static/media/Seeds2.e542cabb.png"
+  },
+  {
+    "revision": "e542cabb75587c13d9aa0a8fc4569222",
+    "url": "/pure-react-animalchemists/static/media/Seeds3.e542cabb.png"
+  },
+  {
+    "revision": "e542cabb75587c13d9aa0a8fc4569222",
+    "url": "/pure-react-animalchemists/static/media/Seeds4.e542cabb.png"
+  },
+  {
+    "revision": "e542cabb75587c13d9aa0a8fc4569222",
+    "url": "/pure-react-animalchemists/static/media/Seeds5.e542cabb.png"
+  },
+  {
+    "revision": "e542cabb75587c13d9aa0a8fc4569222",
+    "url": "/pure-react-animalchemists/static/media/Seeds6.e542cabb.png"
+  },
+  {
+    "revision": "e542cabb75587c13d9aa0a8fc4569222",
+    "url": "/pure-react-animalchemists/static/media/Seeds7.e542cabb.png"
+  },
+  {
+    "revision": "e542cabb75587c13d9aa0a8fc4569222",
+    "url": "/pure-react-animalchemists/static/media/Seeds8.e542cabb.png"
+  },
+  {
+    "revision": "e542cabb75587c13d9aa0a8fc4569222",
+    "url": "/pure-react-animalchemists/static/media/Seeds9.e542cabb.png"
+  },
+  {
+    "revision": "f3437b35f1d6e8a72a55bc4f39cf51d3",
+    "url": "/pure-react-animalchemists/static/media/Silence.f3437b35.png"
+  },
+  {
+    "revision": "dc26fa9ec7f6e4eac6bae728fc09b55d",
+    "url": "/pure-react-animalchemists/static/media/Sloth.dc26fa9e.png"
+  },
+  {
+    "revision": "c1096c12c1161020ebacf757f242a626",
+    "url": "/pure-react-animalchemists/static/media/SlothBack.c1096c12.png"
+  },
+  {
+    "revision": "fce3afbfcbf54194332888a60ed59e26",
+    "url": "/pure-react-animalchemists/static/media/SpellBacks.fce3afbf.png"
+  },
+  {
+    "revision": "582df4a4e149d350d9f47fb7a784b5f4",
+    "url": "/pure-react-animalchemists/static/media/Terraform.582df4a4.png"
+  },
+  {
+    "revision": "2f996ad84bde5d1cf975575c54164a11",
+    "url": "/pure-react-animalchemists/static/media/TimeStop.2f996ad8.png"
+  },
+  {
+    "revision": "c63b3ef756c25dac957124fcf81b99b5",
+    "url": "/pure-react-animalchemists/static/media/TimeStop2.c63b3ef7.png"
+  },
+  {
+    "revision": "1ec33da98c00278eb5ee7349f29c6fa1",
+    "url": "/pure-react-animalchemists/static/media/TimeStopBack.1ec33da9.png"
+  },
+  {
+    "revision": "b2ea766b18b892fb84160a5a29cb4daf",
+    "url": "/pure-react-animalchemists/static/media/Tonic0.b2ea766b.png"
+  },
+  {
+    "revision": "b2ea766b18b892fb84160a5a29cb4daf",
+    "url": "/pure-react-animalchemists/static/media/Tonic1.b2ea766b.png"
+  },
+  {
+    "revision": "b2ea766b18b892fb84160a5a29cb4daf",
+    "url": "/pure-react-animalchemists/static/media/Tonic2.b2ea766b.png"
+  },
+  {
+    "revision": "b2ea766b18b892fb84160a5a29cb4daf",
+    "url": "/pure-react-animalchemists/static/media/Tonic3.b2ea766b.png"
+  },
+  {
+    "revision": "09ae97be86303d48144190d6b6e2aebe",
+    "url": "/pure-react-animalchemists/static/media/TonicBack2.09ae97be.png"
+  },
+  {
+    "revision": "85fc4b149f38b1d517a296cb44770550",
+    "url": "/pure-react-animalchemists/static/media/Transmute.85fc4b14.png"
+  },
+  {
+    "revision": "dc167223792fdbc57ff91963a7da122e",
+    "url": "/pure-react-animalchemists/static/media/pic4767592.dc167223.png"
+  }
+]);
